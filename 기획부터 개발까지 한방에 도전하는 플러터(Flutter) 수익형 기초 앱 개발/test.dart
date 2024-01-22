e78045ed92b0ea0c87e825e49b474777c38312d8
sikomar00 <dolevi4k@gmail.com>
2024-01-22 22:25:00 +0900

    홍드로이드 플러터 2-5 (변수와 자료형)

---
+++ 기획부터 개발까지 한방에 도전하는 플러터(Flutter) 수익형 기초 앱 개발/test.dart
@@ -0,0 +1,23 @@
+// Main 함수 - 프로그램의 시작지점을 가지고 있는 함수
+void main() {
+  String name = '오마르';
+  print(name);
+
+  //정수값 소수점 X
+  int age = 30;
+  print(age);
+
+  var name2 = '홍길동'; //var: dart에서 지원하는 문법으로 변수 값에 대한 타입 추론을 저절로 함(자동으로 str 취급)
+  print(name2);
+
+  var age2 = 25;
+  print(age2);
+
+  //bool : true나 false의 논리를 정의할 떄 사용
+  bool isChecked = false;
+  print(isChecked);
+
+  //double 소수 점 값을 표현하는 단위
+  double height = 164.9;
+  print(height);
+}
